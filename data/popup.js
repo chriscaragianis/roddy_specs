593010ed6c40abdefdbec3255c0eb27f405c34d4
Chris Caragianis <chriscaragianis@gmail.com>
2015-08-08 22:43:36 -0400

    Working on DOM manipulations

--- data/popup.js
+++ data/popup.js
@@ -1,2 +1,24 @@
 $('body').css('filter', 'grayscale(1)');
-alert("diddly piddle");
+
+$.each($('div'), function(i, elt) {
+  var txt = $(elt).text();
+  var position = $(elt).position();
+  var width = $(elt).width();
+  var height = $(elt).height();
+  var wHeight = $(window).height();
+  $(elt).text("Top: " + position.top + "\nLeft: " + position.left + "\nWidth: " + width + "\nHeight: " + height);
+  $(elt).css('position', 'fixed');
+  $(elt).css('top', position.top + "px");
+  $(elt).css('left', position.left + "px");
+  $(elt).css('width', width + "px");
+  if (height > wHeight){
+    height = wHeight;
+  }
+  $(elt).css('height', height + "px");
+  $(elt).css('border', '10px solid #222');
+  $(elt).css('text-align', 'center');
+  $(elt).css('padding-top', '33.33%');
+  $(elt).css('padding-bottom', '33.33%');
+  $(elt).css('font-size', .3333*height + "px"); 
+})
+;
